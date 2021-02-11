5597d3da88832d25bc8e58a1e4ef71e20010db74
WyomingCPA <WyomingCPA@yandex.ru>
2021-02-11 13:48:24 +0300

    fix bug

--- database/migrations/2021_02_11_134054_create_tools_to_candles_table.php
+++ database/migrations/2021_02_11_134054_create_tools_to_candles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTools2ToCandlesTable extends Migration
+class CreateTools22ToCandlesTable extends Migration
 {
     /**
      * Run the migrations.
