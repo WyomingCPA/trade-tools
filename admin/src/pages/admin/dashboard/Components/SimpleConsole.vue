d5637b8595b1061d82169c3b97ee9ab857d22ffe
Nikolay <WyomingCPA@yandex.ru>
2023-08-24 15:01:42 +0300

    work with dashboard

---
+++ admin/src/pages/admin/dashboard/Components/SimpleConsole.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="list-wrapper">
+      <div class="preview-list">
+        <div
+          class="preview-item border-bottom py-3"
+          v-for="event in events"
+          :key="event.id"
+        >
+          <div class="preview-item-content d-flex flex-grow">
+            <div class="flex-grow">
+              <div class="d-sm-flex justify-content-between">
+                <div class="d-flex">
+                  <p class="text-small">{{ event.message }}</p>
+                  <p class="text-small text-muted border-right pr-3">
+                    {{ event.created_at }}
+                  </p>
+                  <b-button
+                    v-b-modal="'modal-' + event.id"
+                    type="submit"
+                    variant="success"
+                    class="mr-2"
+                    ><span> View </span>
+                  </b-button>
+                  <b-modal
+                    size="lg"
+                    :id="'modal-' + event.id"
+                    title=" Modal"
+                    busy
+                  >
+                    <div class="table-responsive">
+                      <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">Figi</th>
+                            <th scope="col">RSI</th>
+                            <th scope="col">RSI Strategy</th>
+                            <th scope="col">MACD</th>
+                            <th scope="col">Last Supertrend</th>
+                            <th scope="col">SuperTrend Strategy</th>
+                            <th scope="col">Time</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="item in event.data" :key="item.id">
+                            <th scope="col">{{ item.figi }}</th>
+                            <th scope="col">{{ item.RSI }}</th>
+                            <th scope="col">{{ item.rsi_strategy_info }}</th>
+                            <th scope="col">{{ item.MACD }}</th>
+                            <th scope="col">{{ item.last_row_supertrend }}</th>
+                            <th scope="col">
+                              {{ item.supertend_strategy_info }}
+                            </th>
+                            <th scope="col">{{ item.time }}</th>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </b-modal>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  import axios from "axios";
+  
+  export default {
+    name: "mini-console",
+    data() {
+      return {
+        timer: null,
+        events: [],
+      };
+    },
+    methods: {
+      getConsoleEvent() {
+        let self = this;
+        this.loading = true;
+        axios
+          .get("/api/console/last-events-console")
+          .then(function (response) {
+            response.data.events.map(function (value, key) {
+              var exists = self.events.some(function (field) {
+                return field.id === value.id;
+              });
+              if (!exists) {
+                value.data = JSON.parse(value.data);
+                console.log(value.data.message);
+  
+                self.events.unshift(value);
+              }
+            });
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      },
+    },
+    mounted: function () {
+      this.timer = setInterval(() => {
+        this.getConsoleEvent();
+      }, 5000);
+    },
+    beforeDestroy() {
+      clearInterval(this.timer);
+    },
+  };
+  </script>
+  <style scoped>
+  .preview-list {
+    height: 400px;
+    overflow-y: scroll;
+  }
+  </style>
--- admin/src/pages/admin/dashboard/DashboardInfoBlock.vue
+++ admin/src/pages/admin/dashboard/DashboardInfoBlock.vue
@@ -1,135 +1,125 @@
 <template>
   <div class="row row-equal">
     <div class="flex xl6 xs12 lg6">
-      <div class="row">
-        <div v-for="(info, idx) in infoTiles" :key="idx" class="flex xs12 sm4">
-          <va-card class="mb-4" :color="info.color">
-            <va-card-content>
-              <h2 class="va-h2 ma-0" style="color: white">{{ info.value }}</h2>
-              <p style="color: white">{{ t('dashboard.info.' + info.text) }}</p>
-            </va-card-content>
-          </va-card>
-        </div>
+      <div class="col-md-4 grid-margin stretch-card">
+        <va-card>
+          <va-card-title>Trade Parser Log</va-card-title>
+          <va-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </va-card-content>
+        </va-card>
       </div>
-
-      <div class="row">
-        <div class="flex xs12 sm6 md6">
-          <va-card>
-            <va-card-content>
-              <h2 class="va-h2 ma-0" :style="{ color: colors.primary }">291</h2>
-              <p class="no-wrap">{{ t('dashboard.info.completedPullRequests') }}</p>
-            </va-card-content>
-          </va-card>
-        </div>
-        <div class="flex xs12 sm6 md6">
-          <va-card>
-            <va-card-content>
-              <div class="row row-separated">
-                <div class="flex xs4">
-                  <h2 class="va-h2 ma-0 va-text-center" :style="{ color: colors.primary }">3</h2>
-                  <p class="va-text-center">{{ t('dashboard.info.users') }}</p>
-                </div>
-                <div class="flex xs4">
-                  <h2 class="va-h2 ma-0 va-text-center" :style="{ color: colors.info }">24</h2>
-                  <p class="va-text-center no-wrap">{{ t('dashboard.info.points') }}</p>
-                </div>
-                <div class="flex xs4">
-                  <h2 class="va-h2 ma-0 va-text-center" :style="{ color: colors.warning }">91</h2>
-                  <p class="va-text-center">{{ t('dashboard.info.units') }}</p>
-                </div>
-              </div>
-            </va-card-content>
-          </va-card>
-        </div>
+      <div class="col-md-4">
+        <va-card>
+          <va-card-title>Trade Parser Log</va-card-title>
+          <va-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </va-card-content>
+        </va-card>
       </div>
     </div>
-
-    <div class="flex xs12 sm6 md6 xl3 lg3">
-      <va-card stripe stripe-color="info">
-        <va-card-title>
-          {{ t('dashboard.info.componentRichTheme') }}
-        </va-card-title>
+  </div>
+  <div class="row">
+    <div class="flex xs12 sm6 md6">
+      <va-card>
         <va-card-content>
-          <p class="rich-theme-card-text">
-            Buying the right telescope to take your love of astronomy to the next level is a big next step.
-          </p>
-
-          <div class="mt-3">
-            <va-button color="primary" target="_blank" href="https://github.com/epicmaxco/vuestic-ui">
-              {{ t('dashboard.info.viewLibrary') }}
-            </va-button>
-          </div>
+          <h2 class="va-h2 ma-0">{{ today_open_orders }}</h2>
+          <p class="no-wrap">Сделки За Сегодня</p>
         </va-card-content>
       </va-card>
     </div>
-
-    <va-modal v-model="modal">
-      <va-carousel v-model="currentImageIndex" :items="images" class="gallery-carousel" />
-    </va-modal>
+    <div class="flex xs12 sm6 md6">
+      <va-card>
+        <va-card-content>
+          <h2 class="va-h2 ma-0">{{ week_open_orders }}</h2>
+          <p class="no-wrap">Сделки За Неделю</p>
+        </va-card-content>
+      </va-card>
+    </div>
+    <div class="flex xs12 sm6 md6">
+      <va-card>
+        <va-card-content>
+          <h2 class="va-h2 ma-0">{{ month_open_orders }}</h2>
+          <p class="no-wrap">Сделки За Месяц</p>
+        </va-card-content>
+      </va-card>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-  import { ref } from 'vue'
-  import { useI18n } from 'vue-i18n'
-  import { VaCarousel, VaModal, VaCard, VaCardContent, VaCardTitle, VaButton, VaImage, useColors } from 'vuestic-ui'
+<script lang="ts">
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { VaCarousel, VaModal, VaCard, VaCardContent, VaCardTitle, VaButton, VaImage, useColors } from 'vuestic-ui'
+import axios from "axios";
 
-  const { t } = useI18n()
-  const { colors } = useColors()
-
-  const infoTiles = ref([
-    {
-      color: 'success',
-      value: '803',
-      text: 'commits',
-      icon: '',
+import { defineComponent } from 'vue'
+export default defineComponent({
+  data() {
+    return {
+      today_open_orders: { type: Number },
+      week_open_orders: { type: Number },
+      month_open_orders: { type: Number },
+      count_all_candles: { type: Number },
+      candles_last_ago: { type: String },
+      count_all_orders: { type: Number },
+      orders_last_ago: { type: String },
+      all_scripts: [],
+      dataUrl: { type: String },
+      loading: false,
+      id_order: 0,
+      serverParams: {},
+      items: [],
+    }
+  },
+  methods: {
+    fetchData() {
+      let self = this;
+      this.loading = true;
+      axios
+        .get("/api/dashboard/index")
+        .then(function (response) {
+          self.today_open_orders = response.data.today_open_orders;
+          self.week_open_orders = response.data.week_open_orders;
+          self.month_open_orders = response.data.month_open_orders;
+          self.count_all_candles = response.data.count_all_candles;
+          self.candles_last_ago = response.data.candles_last_ago;
+          self.count_all_orders = response.data.count_all_orders;
+          self.orders_last_ago = response.data.orders_last_ago;
+          self.all_scripts = response.data.all_scripts;
+          self.loading = false;
+          console.log(response.data.count_all_candles);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
     },
-    {
-      color: 'danger',
-      value: '57',
-      text: 'components',
-      icon: '',
-    },
-    {
-      color: 'info',
-      value: '5',
-      text: 'teamMembers',
-      icon: '',
-    },
-  ])
+  },
+  created() {
+    this.fetchData();
+  },
+})
 
-  const modal = ref(false)
-  const currentImageIndex = ref(0)
-  const images = ref([
-    'https://i.imgur.com/qSykGko.jpg',
-    'https://i.imgur.com/jYwT08D.png',
-    'https://i.imgur.com/9930myH.jpg',
-    'https://i.imgur.com/2JxhWD6.jpg',
-    'https://i.imgur.com/MpiOWbM.jpg',
-  ])
 
-  function showModal() {
-    modal.value = true
-  }
 </script>
 
 <style lang="scss" scoped>
-  .row-separated {
-    .flex + .flex {
-      border-left: 1px solid var(--va-background-primary);
-    }
+.row-separated {
+  .flex+.flex {
+    border-left: 1px solid var(--va-background-primary);
   }
+}
 
-  .rich-theme-card-text {
-    line-height: 1.5;
+.rich-theme-card-text {
+  line-height: 1.5;
+}
+
+.gallery-carousel {
+  width: 80vw;
+  max-width: 100%;
+
+  @media all and (max-width: 576px) {
+    width: 100%;
   }
-
-  .gallery-carousel {
-    width: 80vw;
-    max-width: 100%;
-
-    @media all and (max-width: 576px) {
-      width: 100%;
-    }
-  }
+}
 </style>
