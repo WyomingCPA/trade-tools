c702bb23f779d19d6803169f3ec5f31f331b1906
Nikolay <WyomingCPA@yandex.ru>
2023-09-28 21:13:54 +0300

    work with calculation

---
+++ admin/src/pages/admin/calculators/StockAimAverage.vue
@@ -0,0 +1,68 @@
+<template>
+    <va-card>
+        <va-card-title>Stock Aim Average</va-card-title>
+        <va-card-content>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 items-end">
+                <va-input v-model.number="GeneralPriceStock" type="number" step="any" label="General Price Stock" counter />
+                <va-input v-model.number="GeneralCountStock" type="number" step="any" counter label="General Count Stock" />
+                <va-input v-model.number="AimAveragePrice" type="number" step="any" counter label="Aim average(max) price" />
+                <va-input v-model.number="ThisPriceStock" type="number" step="any" counter label="This stock price" />
+                <va-input v-model.number="Result" type="number" step="any" counter label="Count Result" />
+                <va-button @click="handleCalculate"> Рассчитать </va-button>
+            </div>
+        </va-card-content>
+    </va-card>
+    <div>
+        <b-card title="Average Down Calculator">
+            <b-form @submit="handleCalculate">
+                <b-form-row> </b-form-row>
+                <b-form-row>
+                    <b-col :sm="2"><label class="mr-1">General Price Stock:</label><b-form-input
+                            v-model.number="GeneralPriceStock" type="number" step="any" /></b-col>
+                    <b-col :sm="2"><label class="mr-1">General Count Stock:</label><b-form-input
+                            v-model.number="GeneralCountStock" type="number" step="any" /></b-col>
+                    <b-col :sm="2"><label class="mr-1">Aim average(max) price:</label><b-form-input
+                            v-model.number="AimAveragePrice" type="number" step="any" /></b-col>
+                    <b-col :sm="2"><label class="mr-1">This stock price:</label><b-form-input
+                            v-model.number="ThisPriceStock" type="number" step="any" /></b-col>
+                    <b-col :sm="2"><label class="mr-1">Count Result:</label><b-form-input v-model.number="Result"
+                            type="number" step="any" /></b-col>
+                </b-form-row>
+                <b-form-row class="mt-3">
+                    <b-col :sm="10"><b-btn type="submit" class="btn-success float-md-right">Calculate</b-btn></b-col>
+                </b-form-row>
+            </b-form>
+        </b-card>
+    </div>
+</template>
+<script>
+export default {
+    name: "stock-aim-average-calculator",
+    data() {
+        return {
+            GeneralPriceStock: 0,
+            GeneralCountStock: 0,
+            ThisPriceStock: 0,
+            AimAveragePrice: 0,
+            Result: 0,
+        };
+    },
+
+    methods: {
+        handleCalculate(evt) {
+            evt.preventDefault();
+            let priceStock = this.GeneralPriceStock;
+            let count = 1;
+            while (priceStock >= this.AimAveragePrice || count == 200) {
+                let firstAmountBougt = this.GeneralPriceStock * this.GeneralCountStock;
+                let TotalAmountBought = count * this.ThisPriceStock + firstAmountBougt;
+                let totalCount = count + this.GeneralCountStock;
+                priceStock = TotalAmountBought / totalCount;
+                console.log(priceStock);
+                count++;
+            }
+            this.Result = count;
+        },
+    },
+};
+</script>
